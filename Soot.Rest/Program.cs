57d4872a3729eef26159c0325b535d168b1e7aa5
Mohammad Hadi Bazmaloun <mohammadhadi@smartech.ir>
2022-08-16 15:53:40 +0430

    Update Program.cs

--- Soot.Rest/Program.cs
+++ Soot.Rest/Program.cs
@@ -53,10 +53,11 @@ try
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        
     }
-
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
